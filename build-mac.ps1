a7cab42f7419d864428b838b53d8f3930c131f9d
Lukiya Chen <lukiya@outlook.com>
2021-02-18 13:59:56 -0800

    update

--- build-mac.ps1
+++ build-mac.ps1
@@ -2,10 +2,12 @@ Set-Location ./src/host
 
 $imageName = "redismanager"
 $targetDir = "../../dist/mac/"
-$binPath = $targetDir + $imageName
+$binPathAMD64 = $targetDir + $imageName + "_amd64"
+$binPathARM64 = $targetDir + $imageName + "_arm64"
 
 Write-Host "#: building executable file..."
-$env:GOOS = "darwin"; $env:GOARCH = "amd64"; go build -ldflags="-s -w" -o $binPath ./
+$env:GOOS = "darwin"; $env:GOARCH = "amd64"; go build -ldflags="-s -w" -o $binPathAMD64 ./
+$env:GOOS = "darwin"; $env:GOARCH = "arm64"; go build -ldflags="-s -w" -o $binPathARM64 ./
 # Write-Host "#: compressing executable file..."
 # upx $binPath
 Write-Host "#: copying configs file..."
