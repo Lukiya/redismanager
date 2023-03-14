20f1823974c56b637764ccaed2c9d3be441f45d6
Lukiya <lukiya@outlook.com>
2023-03-14 08:11:31 -0700

    add timeout for js

--- src/gui/src/services/generalAPI.ts
+++ src/gui/src/services/generalAPI.ts
@@ -2,7 +2,9 @@ import u from '@/u';
 import { request } from 'umi';
 
 export async function GetInfo() {
-    const r = await request(u.LocalRootURL() + "api/info");
+    const r = await request(u.LocalRootURL() + "api/info", {
+        timeout: 3000,
+    });
 
     return r;
 }
